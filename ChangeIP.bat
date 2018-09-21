5bd002a15bdf14bf782ba99cebead2de8f1f8405
Jason <465317110@qq.com>
2018-09-20 18:35:54 -0700

    Update ChangeIP.bat

--- ChangeIP.bat
+++ ChangeIP.bat
@@ -1,11 +1,11 @@
 @echo off      
 cls           
 color 0A 
-echo IP地址更改工具
+echo IPµØÖ·¸ü¸Ä¹¤¾ß
 
 ::ver 0.0921
-::修正DNS部分代码
-::0921-2018 09:21	@张志超	
+::ÐÞÕýDNS²¿·Ö´úÂë
+::0921-2018 09:21	@ÕÅÖ¾³¬	
 
 set MASK=255.255.255.0 
 set GATEWAY=192.168.0.1 
@@ -17,11 +17,11 @@ set DNS2=219.150.32.132
 
 echo.
 echo. 
-echo 默认配置信息为：
+echo Ä¬ÈÏÅäÖÃÐÅÏ¢Îª£º
 echo.
-echo 子网掩码:	%MASK%
-echo 网关:		%GATEWAY%
-echo 适配器:		%NAME%
+echo ×ÓÍøÑÚÂë:	%MASK%
+echo Íø¹Ø:		%GATEWAY%
+echo ÊÊÅäÆ÷:		%NAME%
 echo DNS1:		%DNS1%
 echo DNS2:		%DNS2%
 echo.
@@ -31,26 +31,26 @@ echo.
  
 echo. 
 echo. 
-echo 1:更改IP  
-echo 2:显示网络配置信息
-echo 0:退出
+echo 1:¸ü¸ÄIP  
+echo 2:ÏÔÊ¾ÍøÂçÅäÖÃÐÅÏ¢
+echo 0:ÍË³ö
 
 echo.
-set /p KEY= [您的选择是：]     
+set /p KEY= [ÄúµÄÑ¡ÔñÊÇ£º]     
 if %KEY% == 1 goto CASE1
 if %KEY% == 2 goto CASE2
 if %KEY% == 0 goto EXIT
 
 :CASE1
 echo.
-set /p IP= [请输入IP地址:] 
+set /p IP= [ÇëÊäÈëIPµØÖ·:] 
 echo. 
-echo 正在更改IP.....
+echo ÕýÔÚ¸ü¸ÄIP.....
 netsh interface ip set address name=%NAME% source=static addr=%IP% mask=%MASK% gateway=%GATEWAY% 
 netsh interface ip set dns %NAME% static %DNS1%
 netsh interface ip add dns %NAME% %DNS2%
 
-echo IP地址/子网掩码/网关设置完成 
+echo IPµØÖ·/×ÓÍøÑÚÂë/Íø¹ØÉèÖÃÍê³É 
 goto ENTER
 
 :CASE2
